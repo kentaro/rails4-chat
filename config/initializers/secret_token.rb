b7e798447c8372da69555b35ffa1fca118c0100f
Kentaro Kuribayashi <kentarok@gmail.com>
2012-11-14 21:46:55 +0900

    rails new --edge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_token = '423e8828ad4a4649d2e454a074dfca4d0c92e7c1dec16a4f834de3109f90f1f9c83c4420285aaf0b776d8b109909c2ac725776cc820c47b59fe58ee59c532e89'
